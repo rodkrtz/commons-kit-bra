f7ff742a44145a7f0e6fe630e7cbf782f31f0498
rodkrtz <rodkrtz@gmail.com>
2026-03-02 14:18:32 -0300

    ajustes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,21 @@
 plugins {
     kotlin("jvm") version "2.3.0"
+    `maven-publish`
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
 }
 
 group = "com.rodkrtz"
 version = "0.0.1"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
